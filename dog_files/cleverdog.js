fde7ae253c8c134d5ed3932c57bf4c0923887625
jamesvan <jennyma228@hotmail.com>
2015-08-05 11:06:03 +0800

    first commit

---
+++ dog_files/cleverdog.js
@@ -0,0 +1,214 @@
+function uploadTxt(page)
+{
+  var formParam = $("#pageText").serialize(); 
+  $.ajax({
+     type:"POST",
+     url:"/action/uploadTexts?mid="+page.value,
+     dataType:"json",
+     data:formParam,
+     success:function(data){
+       $("#mytitleDiv").html(data.mytitle);
+       $("#mytextDiv").html(data.mytext);
+     }
+  })
+}
+
+function uploadComment(page)
+{
+  var formParam = $("#myComment").serialize(); 
+  $.ajax({
+     type:"POST",
+     url:"/action/uploadTexts?mid="+page.value,
+     dataType:"json",
+     data:formParam,
+     success:function(data){
+      loadComment("top");
+     }
+  })
+}
+
+function deleteComment(page)
+{
+  $.ajax({
+     type:"POST",
+     url:"/action/deleteComment?mid="+page,
+     dataType:"json",
+     data:"{commentid:"+page+"}",
+     success:function(data){
+      $("#a"+data.commentid).parent().remove();
+     }
+  })
+}
+
+function loginwebsite()
+{
+  var formParam = $("#logmein").serialize(); 
+  $.ajax({
+    type:"POST",
+    url:"/exe/login",
+    dataType:"json",
+    data:formParam,
+    success:function(data){
+      $(".login").hide();
+
+      $(".uploader").show();
+      if($(".author").html()==data.auth){
+        $(".contentbar .delete").show();
+        $(".textform").show();
+      }
+
+      $("#user").val(data.auth);
+      $("#user").attr("showpage","visible");
+
+      $(".commentform").show();
+      {
+        $(".commentlist").html("");
+        $("#comcount").attr("buttom","1024");
+        loadComment("buttom");
+      }
+    },
+    complete:function(XMLHttpRequest,textStatus){
+    },
+    error:function(e){
+      $("#loginerror").html("登录不成功,请重试!");
+    }
+  })
+}
+
+function editertoggle(){
+  var user=$("#user").val();
+  var auth=$(".author").html();
+  var pageshow=$("#user").attr("showpage");
+  if(pageshow=="hidden"){
+    if(user=="none")
+    {
+      $(".login").show();
+    }
+    else
+    {
+      $(".uploader").show();
+      $(".commentform").show();
+      if(user==auth)
+      {
+        $(".textform").show();
+        $(".contentbar .delete").show();
+      }
+      $(".content .delete").show();
+    }
+    $("#user").attr("showpage","visible");
+  }else{
+    if(user=="none")
+    {
+      $(".login").hide();
+    }
+    else
+    {
+      $(".uploader").hide();
+      $(".commentform").hide();
+      if(user==auth)
+      {
+        $(".textform").hide();
+        $(".contentbar .delete").hide();
+      }
+      $(".content .delete").hide();
+    }
+    $("#user").attr("showpage","hidden");
+  }
+}
+
+$(document).ready(function(){
+  $(".gochannels").click(function(){
+    editertoggle();
+  });
+  
+  $(".gochannel").click(function(){
+    loadComment("buttom");
+  });
+  $("#go").click(function(){
+    loginwebsite();
+  });
+});
+
+function getJsonLength(jsonData){
+  var jsonLength = 0;
+  for(var item in jsonData){
+    jsonLength++;
+  }
+  return jsonLength;
+}
+
+var timeoutProcess;
+
+function loadComment(gettop)
+{
+  clearTimeout(timeoutProcess);
+  var top=parseInt($("#comcount").attr("top"));
+  var buttom=parseInt($("#comcount").attr("buttom"));
+  var mid=parseInt($("#comcount").attr("lst_id"));
+  var pageshow=$("#user").attr("showpage");
+  //alert("gettop:"+gettop+" mid:"+mid+" top:"+top+" buttom:"+buttom);
+  if(gettop=="top"){
+    var req="&top="+top;
+  }else{
+    var req="&buttom="+buttom;
+  }
+
+  $.ajax({
+    type:"POST",
+    url:"/exe/getComment?mid="+mid+req,
+    dataType:"json",
+    data:{hear:$("#head").val()},
+    success:function(data){
+      var obj = eval(data);
+      var total=getJsonLength(obj.comments);
+      var comment="";
+    
+      if(total>0){
+        for(var i=0;i<total;i++) {
+          comment+='<li><h3 class="meta" id=a'+obj.comments[i].id+'>'+obj.comments[i].author
+          +'<time>'+obj.comments[i].txt_time +'</time></h3>'
+          +'<p class="content">'+obj.comments[i].txt_title;
+          if($("#user").val()==obj.comments[i].author){
+            comment+='<span class="commentbar">'
+            +'<a href="javascript:deleteComment('+obj.comments[i].id+')" ><i class="delete"'+pageshow+' ></i></a>'
+            +'</span></p>';
+          }
+          comment+='</p>\r\n</li>\r\n'; //
+        }
+        var nowtop=obj.comments[0].id;
+        var nowbuttom=obj.comments[total-1].id;
+        if(gettop=="top"){
+          $(".commentlist").prepend(comment);
+          if(nowtop!=top){
+            $("#comcount").attr("top",nowtop);
+          }
+          if(buttom==1024){
+            $("#comcount").attr("buttom",nowbuttom);
+          }
+        }else{
+          $(".commentlist").append(comment);
+          if(nowbuttom!=buttom){
+            $("#comcount").attr("buttom",nowbuttom);
+          }
+          if(top==1){
+            $("#comcount").attr("top",nowtop);
+          }
+        }
+        //alert("top="+obj.comments[0].id+" buttom"+obj.comments[total-1].id);
+      }
+      timeoutProcess=setTimeout("loadComment(\"top\")",10000);
+    },
+    complete:function(XMLHttpRequest,textStatus){
+      timeoutProcess=setTimeout("loadComment(\"top\")",10000);
+    },
+    error:function(e){
+      $("#head").html("<h2>全部评论</h2>");
+    }
+  })
+}
+
+function replaceDoc(url)
+{
+  //alert(url);
+  window.location.replace(url);
+}
---
+++ dog_files/touch.js
@@ -0,0 +1,70 @@
+//全局变量，触摸开始位置
+var startX = 0, startY = 0;
+
+//touchstart事件
+function touchSatrtFunc(evt) {
+  try {//evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+      var touch = evt.touches[0]; //获取第一个触点 
+      startX = Number(touch.pageX); //页面触点X坐标 记录触点初始位置
+      startY = Number(touch.pageY); //页面触点Y坐标 记录触点初始位置
+  }
+  catch (e) {//alert('touchSatrtFunc：' + e.message);
+  }
+}
+
+//touchmove事件
+function touchMoveFunc(evt) {
+  try {//evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+    var touch = evt.touches[0]; //获取第一个触点
+    var difx=Number(touch.pageX) - startX; //页面触点X坐标
+    var dify=Number(touch.pageY) - startY; //页面触点Y坐标
+    //判断滑动方向
+    if (Math.abs(difx) > 80) {
+      if(difx==Math.abs(difx)){
+        //alert('向右滑动');
+        replaceDoc("./exe/nextPage?mid="+$("#comcount").attr("lst_id")+"&next_last=0");
+      } else {
+        //alert('向左滑动');
+        replaceDoc("./exe/nextPage?mid="+$("#comcount").attr("lst_id")+"&next_last=1");
+      }
+    }
+    if (Math.abs(dify) > 80) {
+      if(dify==Math.abs(dify)){
+        //alert('向下滑动');
+      } else {
+        //alert('向上滑动');
+      }
+    }
+  }
+  catch (e) { //alert('touchMoveFunc：' + e.message);
+  }
+}
+
+//touchend事件
+function touchEndFunc(evt) {
+  try {//evt.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
+  }
+  catch (e) {//alert('touchEndFunc：' + e.message);
+  }
+}
+
+//绑定事件
+function bindEvent() {
+  document.addEventListener('touchstart', touchSatrtFunc, false);
+  document.addEventListener('touchmove', touchMoveFunc, false);
+  document.addEventListener('touchend', touchEndFunc, false);
+}
+
+//判断是否支持触摸事件
+function isTouchDevice() {
+  //alert(navigator.appVersion);
+  try { //alert("支持TouchEvent事件！");
+    document.createEvent("TouchEvent");
+    bindEvent(); //绑定事件
+  }
+  catch (e) { //alert("不支持TouchEvent事件！" + e.message);
+  }
+}
+
+window.onload = isTouchDevice;
+
